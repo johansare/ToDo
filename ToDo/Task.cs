8cd459bf7e257c58696869aabe38d983c876df10
jecs <johansare@gmail.com>
2025-06-13 23:32:33 +0200

    Change String to string in ToDo class

--- ToDo/Task.cs
+++ ToDo/Task.cs
@@ -5,7 +5,7 @@
         public string Title { get; set; }
         public DateTime Date { get; set; }
         public bool IsCompleted { get; set; }
-        public String Project { get; set; }
+        public string Project { get; set; }
 
         public Task(string title, string project, DateTime date, bool isCompleted)
         {
